3e041c8a00af7ece65ee14a194606bff3ecba707
Lemon <lemon2589@163.com>
2026-06-03 18:28:12 +0800

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -5,7 +5,13 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from tools import get_goods_info, search_products, get_shipping_info, control_stock, query_order
 from langgraph.checkpoint.memory import InMemorySaver
-load_dotenv()
+
+if "DEEPSEEK_API_KEY" in st.secrets:
+    os.environ["DEEPSEEK_API_KEY"] = st.secrets["DEEPSEEK_API_KEY"]
+    os.environ["DEEPSEEK_BASE_URL"] = st.secrets["DEEPSEEK_BASE_URL"]
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 def init_agent():
     now = datetime.datetime.now()
